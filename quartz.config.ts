03758a5330c35b2396d05e9cbe2b9ee6350fc0a5
shamusmurphy <smurph46@uvm.edu>
2025-12-19 17:38:32 -0500

    Quartz sync: Dec 19, 2025, 5:38 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "shamusmurphy.xyz",
+    baseUrl: "lab.shamusmurphy.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
