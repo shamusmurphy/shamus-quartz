56e3fc3096a53f8d8b72f49d1e9f88e90a1e5586
shamusmurphy <smurph46@uvm.edu>
2025-12-18 23:40:05 -0500

    Quartz sync: Dec 18, 2025, 11:40 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -49,7 +49,10 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ConditionalRender({
+      component: Component.ContentMeta(),
+      condition: (page) => page.fileData.slug !== "index",
+    }),],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
