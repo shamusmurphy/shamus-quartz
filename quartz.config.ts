7385bc43dabef5de9a34c2bd15ebf37c30f27681
shamusmurphy <smurph46@uvm.edu>
2026-02-17 15:38:33 -0500

    changed config

--- quartz.config.ts
+++ quartz.config.ts
@@ -74,6 +74,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.HardLineBreaks(),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
